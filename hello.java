81b8908535b942fbae246808aa079f6bb104b783
mohitesheetal22 <94891303+mohitesheetal22@users.noreply.github.com>
2022-03-07 14:19:18 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,11 +1,1 @@
-public class hello{
-    public static void main(String[] args)
-       {
-         for(int i=1;i<=10;i++){
-
-            System.out.println("hello World");
-           
-           }
-       
-}
-}
+echo "hello world"
